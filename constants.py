f780b135b8227648370ba7aa9c26c306a19caf4d
Upptech1 <upptech1@gmail.com>
2025-09-18 14:15:05 +0200

    Baesar version

--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
 # Problem constants
-BOARD_SIZE = 8   # Board size (try 8, 20, 50...)
+BOARD_SIZE = 8
 
 # Evolutionary constants
-POPULATION_SIZE = 200
-GENERATIONS = 5000
+POPULATION_SIZE = 10
+MAXIMUM_GENERATIONS = BOARD_SIZE**3
 ELITISM = 2
 TOURNAMENT_K = 4
 SELECTION_SIZE = 0.75
 CROSSOVER_RATE = 0.9
 MUTATION_RATE = 0.2
 
-# Optional wall-clock time limit (in seconds)
+# wall-clock time limit (in seconds)
 TIME_LIMIT_S = None
