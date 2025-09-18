7dab38ac95bfdd5ed80dbbfd84e6337a338898a6
Ahmed Hussein <huah21vn@gmail.com>
2025-09-18 11:31:13 +0200

    push this

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 import board
 
 # Problem constants
-board_size = 8
+BOARD_SIZE = 8
 
 # Evolutionary constants
 population_size = 10
 mutation_rate = 0.1
 
 # Initialize first generation's samples
-boards = [board.Board(board_size) for x in range(population_size)]
+boards = [board.Board(BOARD_SIZE) for x in range(population_size)]
 
 for board in boards:
     print(board.state, board.number_of_attacks)
 
-visual_table = []
 
+def fill_table():
+    table = []
+    for i in range(BOARD_SIZE):
+        # Create a new list for each row
+        row = [" "] * BOARD_SIZE
+        table.append(row)
+    return table
+visual_table = fill_table()
+
+
+def get_table (board, table):
+  
+    for i in range(BOARD_SIZE):
+        table[board.state[i]][i] = "Q"
+    return table
+def print_console_table (table):
+
+    for i in range(BOARD_SIZE):
+        print(table[i])
+
+visual_table = get_table(boards[1], visual_table)
+print_console_table(visual_table)
+            
+
+ 
