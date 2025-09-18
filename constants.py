8b9bf74cd016397855f58198f465da325661c996
hannan khalil <khha22zo@student.ju.se>
2025-09-18 12:40:58 +0200

    modified contants file

--- constants.py
+++ constants.py
@@ -1,6 +1,13 @@
-# Problem constants
-BOARD_SIZE = 8
+# Problem constants 
+BOARD_SIZE = 8   
 
 # Evolutionary constants
-POPULATION_SIZE = 10
-MUTATION_RATE = 0.1
+POPULATION_SIZE = 200
+GENERATIONS = 5000
+ELITISM = 2
+TOURNAMENT_K = 4
+CROSSOVER_RATE = 0.9
+MUTATION_RATE = 0.2
+
+# wall-clock time limit (in seconds)
+TIME_LIMIT_S = None
